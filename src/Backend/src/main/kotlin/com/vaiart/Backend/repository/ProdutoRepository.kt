ad954022ac6733b30aecd74a1eaf9dd213ea129d
Geovanne do Monte <geovannedomonte@gmail.com>
2025-04-25 13:22:26 -0300

    Criando a interface  ProdutoRepository.kt

---
+++ src/Backend/src/main/kotlin/com/vaiart/Backend/repository/ProdutoRepository.kt
@@ -0,0 +1,13 @@
+package com.vaiart.Backend.repository
+
+import com.vaiart.Backend.model.Produto
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProdutoRepository: JpaRepository<Produto, Long> {
+    fun findByNomeContainingIgnoreCase(nome: String, pageable: Pageable): Page<Produto>
+    fun findByDisponivelTrue(pageable: Pageable): Page<Produto>
+}
