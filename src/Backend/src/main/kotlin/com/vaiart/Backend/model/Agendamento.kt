bc6ce66605f2953a4c0d8e62352e80903e9b24e4
Geovanne do Monte <geovannedomonte@gmail.com>
2025-04-25 12:49:56 -0300

    Criando a classe Agendamento.kt

---
+++ src/Backend/src/main/kotlin/com/vaiart/Backend/model/Agendamento.kt
@@ -0,0 +1,26 @@
+package com.vaiart.Backend.model
+
+import  jakarta.persistence.*
+import java.time.LocalDateTime
+
+data class Agendamento(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(name = "nome_cliente", nullable = false)
+    val nomeCliente: String,
+
+    @Column(name = "telefone_cliente", nullable = false)
+    val telefoneCliente: String,
+
+    @Column(name = "data_hora", nullable = false)
+    val dataHora: LocalDateTime,
+
+    @Column(nullable = false)
+    val endereco: String,
+
+    @Column
+    val observacoes: String? = null
+)
